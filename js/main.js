6ba836145766e0e5ccb863d1dc347e8244c8399a
Wren <wrenbernstein@gmail.com>
2019-04-17 16:18:31 -0400

    i put a gif and images folder in bc yaaay
    also i added some small things that can be changed later

--- js/main.js
+++ js/main.js
@@ -3,10 +3,14 @@ const app = new Vue({
     data: {
         title: "Fetch",
         results: [],
-        num: 0
+        num: 0,
+        gif: ""
     },
     methods: {
         search() {
+            gif=`<img src="images/Double Ring-1.5s-200px.gif`;
+
+
             let pf = new petfinder.Client({ apiKey: "3aPqyYam1lM9nzOX5yAUempjnMNDApTMvEwCr8VSwV4RX8j0OK", secret: "LzTl7yGbikkCB9Cx5yBr3vIfUyGl7bmCdLp3JAXT" });
 
             pf.animal.search()
@@ -17,6 +21,7 @@ const app = new Vue({
                 .catch(function (e) {
 
                 })
+                
 
 
         }
